363dfac0a8a70d5ccafb04cfac1a5e6813c4fa57
jodroassis <joao.santos006@sptech.school>
2025-09-28 00:51:55 -0300

    refatorando categoria (dto, mapper, service e controller)

--- java/src/main/java/com/toomate/backend/controller/CategoriaController.java
+++ java/src/main/java/com/toomate/backend/controller/CategoriaController.java
@@ -1,9 +1,13 @@
 package com.toomate.backend.controller;
 
+import com.toomate.backend.dto.categoria.CategoriaMapperDto;
+import com.toomate.backend.dto.categoria.CategoriaRequestDto;
+import com.toomate.backend.dto.categoria.CategoriaResponseDto;
 import com.toomate.backend.model.Categoria;
-import com.toomate.backend.repository.CategoriaRepository;
+import com.toomate.backend.service.CategoriaService;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.EntityResponse;
@@ -15,22 +19,24 @@ import java.util.List;
 @RequestMapping("/categorias")
 public class CategoriaController {
 
-    private final CategoriaRepository repository;
+    private final CategoriaService categoriaService;
 
-    public CategoriaController(CategoriaRepository repository){this.repository = repository;}
+    public CategoriaController(CategoriaService categoriaService){this.categoriaService = categoriaService;}
 
     @GetMapping
-    public ResponseEntity<List<Categoria>> listar(){
-        List<Categoria> categorias = repository.findAll();
-        if(categorias.isEmpty()){
+    public ResponseEntity<List<CategoriaResponseDto>> listar(){
+        List<CategoriaResponseDto> dtos = CategoriaMapperDto.toResponseDto(categoriaService.listar());
+
+        if (dtos.isEmpty()) {
             return ResponseEntity.status(204).build();
         }
-        return ResponseEntity.status(200).body(categorias);
+
+        return ResponseEntity.status(200).body(dtos);
     }
 
     @GetMapping("/por-nome")
     public ResponseEntity<List<Categoria>> filtroNome(@RequestParam String nome){
-        List<Categoria> categorias = repository.findByNomeContainingIgnoreCase(nome);
+        List<Categoria> categorias = categoriaService.listarPorNome(nome);
         if(categorias.isEmpty()){
             return ResponseEntity.status(204).build();
         }
@@ -38,28 +44,25 @@ public class CategoriaController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> cadastrar(@RequestBody Categoria categoria){
-        if(repository.existsByNome(categoria.getNome())){
+    public ResponseEntity<Categoria> cadastrar(@Valid @RequestBody CategoriaRequestDto categoria){
+        if(categoriaService.existePorNome(categoria.getNome())){
             return ResponseEntity.status(409).build();
         }
-        repository.save(categoria);
+        categoriaService.cadastrar(categoria);
         return ResponseEntity.status(201).build();
     }
 
-    @DeleteMapping("/{nome}")
-    public ResponseEntity<Void> deletar(@PathVariable String nome){
-        if(repository.existsByNome(nome)){
-            repository.deleteById(repository.findByNome(nome).getIdCategoria());
-            return ResponseEntity.status(204).build();
-        }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id){
+        categoriaService.deletar(id);
         return ResponseEntity.status(404).build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> atualizar(@PathVariable Integer id, @RequestBody Categoria categoria){
+    public ResponseEntity<Categoria> atualizar(@PathVariable Integer id, @RequestBody Categoria categoria){
         categoria.setIdCategoria(id);
-        if(repository.existsById(id)){
-            repository.save(categoria);
+        if(categoriaService.existePorId(id)){
+            categoriaService.atualizar(id, categoria);
             return ResponseEntity.status(200).build();
         }
         return ResponseEntity.status(404).build();
