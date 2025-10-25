fa1b001661974aa2efe9c1c452f5049d2d3242a7
lucaspaessptech <lucas.paes@sptech.school>
2025-10-25 20:12:36 -0300

    Ajuste no código de erro da exceção de conflito

--- java/src/main/java/com/toomate/backend/exceptions/RecursoExisteException.java
+++ java/src/main/java/com/toomate/backend/exceptions/RecursoExisteException.java
@@ -3,7 +3,7 @@ package com.toomate.backend.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class RecursoExisteException extends RuntimeException {
     public RecursoExisteException(String message) {
         super(message);
