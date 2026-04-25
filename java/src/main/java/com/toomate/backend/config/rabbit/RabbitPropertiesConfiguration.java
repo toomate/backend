82b04d450f19142546db3adcebef2ec1e7e999de
DinizSptech <mateus.leite@sptech.school>
2026-04-25 05:01:48 -0300

    Outros fix no produtor

--- java/src/main/java/com/toomate/backend/config/rabbit/RabbitPropertiesConfiguration.java
+++ java/src/main/java/com/toomate/backend/config/rabbit/RabbitPropertiesConfiguration.java
@@ -1,10 +1,13 @@
 package com.toomate.backend.config.rabbit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "broker")
 public record RabbitPropertiesConfiguration(
+        @Value("${broker.exchange.name}")
         Exchange exchange,
+        @Value("${broker.queue.name}")
         Queue queue
 ) {
     public record Exchange(String name) {
